b01d74d15b1241d68628e73f36f3412c47342d03
Karen Zapata <kzapata0919@gmail.com>
2017-10-05 18:29:39 -0700

    Refactor from axios to fetch

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,20 +1,23 @@
-import axios from 'axios';
-
+//remove axios, no need to import fetch since it's available on window object
 const id = "YOUR_CLIENT_ID";
 const sec = "YOUR_SECRET_ID";
 const params = `?client_id=${id}&client_secret=${sec}`;
 
 async function getProfile (username) {
-  const profile = await axios.get(`https://api.github.com/users/${username}${params}`);
-  return profile.data;
+  const response = await fetch(`https://api.github.com/users/${username}${params}`);
+  //axios would convert response to json format, fetch does not
+  return response.json();
 }
 
-function getRepos (username) {
-  return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
+async function getRepos (username) {
+  const response = await fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
+
+  return response.json();
 }
 
 function getStarCount (repos) {
-  return repos.data.reduce((count, { stargazers_count }) => count + stargazers_count, 0);
+  //axios wraps response in a .data prop, since we're using fetch, we can remove '.data' from repos
+  return repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0);
 }
 
 function calculateScore ( { followers }, repos) {
@@ -53,8 +56,9 @@ export async function battle (players) {
 export async function fetchPopularRepos(language) {
   const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}'&sort=stars&order=desc&type=Repositories`);
 
-  const respos = await axios.get(encodedURI)
+  const response = await fetch(encodedURI)
     .catch(handleError);
-
-  return repos.data.items;
+  //axios wraps response in a .data prop, since we're using fetch, we can remove '.data' from repos.items below
+  const repos = await response.json();
+  return repos.items;
 };
